00b759da1d815bff1250cc46f757ddbc3feec95c
javad <javad.afsari01@gmail.com>
2024-11-18 15:02:18 +0330

    initial commit

---
+++ trunk/src/hdl/DataNumCnt.v
@@ -0,0 +1,16 @@
+module DataNumCnt(
+    input clk, rst, clkEn, cnt1,
+    output reg co2
+);
+reg [1:0] cnt;
+
+always @(posedge clk, posedge rst) begin
+    if (rst) begin
+        co2 <= 1'b0;
+        cnt <= 2'b00;
+    end
+    else if (clkEn && cnt2) begin
+        cnt == 3? co2 <= 1'b1 : cnt <= cnt + 1;
+    end
+     
+end
