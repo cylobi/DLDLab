8ccefbfe8bc01f6b3b06a24cbf3755af2f7f5709
javad <javad.afsari01@gmail.com>
2024-11-25 13:38:10 +0330

    changed next stage block

--- trunk/src/hdl/controller.v
+++ trunk/src/hdl/controller.v
@@ -13,7 +13,7 @@ module Controller(input clk,
 				Sh_enD,
 				SerOutValid,
 				Done);
-				
+
 	parameter Idle = 3'b000,
 	 		  red = 3'b001,
 			  green = 3'b010,
@@ -23,13 +23,15 @@ module Controller(input clk,
 	
 	always @(ps, SerIn, Co1, Co2, CoD)begin
 		case(ps)
-		Idle: ns = (SerIn && serIn) ? Idle : red;
-		red: ns = Co1 ? green : red;
-		green: ns = Co2 ? transmit_Data : green;
-		black: ns = SerIn ? Idle : red;
-		default ns = Idle;
+			Idle: ns = SerIn ? Idle : red;
+			red: ns = Co1 ? green : red;
+			green: ns = Co2 ? black : green;
+			black: ns = coD ? Idle : red;
+			default ns = Idle;
 		endcase
 	end
+
+	// add signal blcok
 
 
 	always @(posedge clk, posedge rst)begin
