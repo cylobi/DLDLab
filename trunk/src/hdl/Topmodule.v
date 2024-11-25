f063d88d89726c986b8f5752e0ea70025e9b4755
javad <javad.afsari01@gmail.com>
2024-11-25 15:28:55 +0330

    fixed some bugs

--- trunk/src/hdl/Topmodule.v
+++ trunk/src/hdl/Topmodule.v
@@ -5,9 +5,9 @@ module Topmodule(
 
 	wire Co1, Co2, CoD, Cnt1, Cnt2, CntD, ldcntD, Sh_enP, Sh_enD;
 
-	Controller C(.clk(clk), .clkEn(clkEn), .rst(rst), .serIn(SerIn), .Co1(Co1), .co2(Co2), .CoD(CoD), .Cnt1(Cnt1), .Cnt2(Cnt2), .CntD(CntD), .ldcntD(ldcntD),
-			 .Sh_enP(Sh_enP), .sh_enD(Sh_enD), .SerOutValid(SerCntValid), .Done(Done));
+	Controller C(.clk(clk), .clkEn(clkEn), .rst(rst), .serIn(SerIn), .Co1(Co1), .Co2(Co2), .CoD(CoD), .Cnt1(Cnt1), .Cnt2(Cnt2), .CntD(CntD), .ldcntD(ldcntD),
+			 .Sh_enP(Sh_enP), .Sh_enD(Sh_enD), .SerOutValid(SerCntValid), .Done(Done));
 
-	Datapath D(.clk(clk), .clkEn(clkEn), .rst(rst), .serIn(SerIn), .cnt1(Cnt1), .cnt2(Cnt2), .cntD(CntD), .ldcntD(ldcntD), .shEn(Sh_enP), .shEnD(Sh_enD), 
-			.serCntValid(serCntValid), .Done(Done), .co1(Co1), .co2(Co2), .CoD(CoD), .p0(P0), .p1(P1), .p2(P2), .p3(P3), .SSDout(SSD_Out));
+	Datapath D(.clk(clk), .clkEn(clkEn), .rst(rst), .serIn(SerIn), .cnt1(Cnt1), .cnt2(Cnt2), .cntD(CntD), .ldCntD(ldcntD), .shEn(Sh_enP), .shEnD(Sh_enD), 
+			.serCntValid(serCntValid), .Done(Done), .co1(Co1), .co2(Co2), .coD(CoD), .p0(P0), .p1(P1), .p2(P2), .p3(P3), .SSDout(SSD_Out));
 endmodule
