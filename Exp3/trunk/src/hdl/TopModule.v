d9804c16798c49309b1bce98f9f7c9da2e4c54b5
Mohammad javad Afsari <javad.afsari01@gmail.com>
2024-12-30 13:56:32 +0330

    top mo

--- Exp3/trunk/src/hdl/TopModule.v
+++ Exp3/trunk/src/hdl/TopModule.v
@@ -8,3 +8,26 @@ module TopModule(
     output out
 )
 
+wire dds_out;
+wire pwm_in;
+wire pwm_out;
+
+DDS dds(
+    .clk(),
+    .rst(rst),
+    .Magnitude(dds_out)
+);
+
+Mux_2_to_1_8bit m8b2i1(
+    .SM(),
+    .a(8'd128),
+    .b(dds_out),
+    .out(pwm_in)
+);
+
+PWM pwm(
+    .clk(clk),
+    .rst(rst),
+    .par_in(pwm_in),
+    .par_out(out)
+)
