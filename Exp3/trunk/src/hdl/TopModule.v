5f9c7095ca833657307aa8d9134b004efd2f24d0
parsa alizadeh <p.alizadeh1383@gmail.com>
2024-12-30 13:53:58 +0330

    top

--- Exp3/trunk/src/hdl/TopModule.v
+++ Exp3/trunk/src/hdl/TopModule.v
@@ -3,8 +3,20 @@ module TopModule(
     input [4:0] msg,
     input rst,
     input init,
-    input [2:0]sw,
+    input [8:0]sw,
     input mode,
     output out
-)
+);
 
+    wire out_frq;
+
+    Top_frq tf(
+        .clk(clk),
+        .rst(rst),
+        .sel(),
+        .msg(msg),
+        .SW(SW[7:5]),
+        .out(out_frq)
+    );
+
+endmodule
